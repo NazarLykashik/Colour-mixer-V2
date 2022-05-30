667d37c7c7ee57688b0cf97773e650d7341bb0a4
NazarLukashik <nazarlykashik@gmail.com>
2022-05-31 00:29:08 +0300

    everything work except one func which can change text field

--- Colour-mixer V2/ViewController.swift
+++ Colour-mixer V2/ViewController.swift
@@ -22,8 +22,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var greenTextField: UITextField!
     @IBOutlet weak var blueTextField: UITextField!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        addDoneButtonTo(redTextField)
+        addDoneButtonTo(greenTextField)
+        addDoneButtonTo(blueTextField)
         
         //скругление углов
         colorView.layer.cornerRadius = 15
@@ -77,3 +82,68 @@ class ViewController: UIViewController {
     }
 }
 
+extension ViewController: UITextFieldDelegate{
+    //скрываем клавиатуру на done
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+
+    //скрытие клавиатуры по тапу на экран за пределами тест вью
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true) //скрывает клавиатуру, вызванную для любого объекта
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        guard let text = textField.text else { return }
+        
+        if let currentValue = Float(text) {
+            
+            switch textField.tag {
+            case 0: redSlider.value = currentValue
+            case 1: greenSlider.value = currentValue
+            case 2: blueSlider.value = currentValue
+            default: break
+            }
+            setColor()
+            setValueForLabel()
+        }else{
+            showAlert(title: "Wrong format!", message: "Please enter correct value")
+        }
+     }
+}
+
+extension ViewController {
+    // отображаем кнопку готово в клавиатуре
+    private func addDoneButtonTo(_ textField: UITextField) {
+        
+        let keyboardToolbar = UIToolbar()
+        textField.inputAccessoryView = keyboardToolbar
+        keyboardToolbar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(title:"Done",
+                                         style: .done,
+                                         target: self,
+                                         action: #selector(didTapDone))
+        
+        let flexBarButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace,
+                                            target: nil,
+                                            action: nil)
+        
+        
+        
+        keyboardToolbar.items = [flexBarButton, doneButton]
+    }
+    
+    @objc private func didTapDone() {
+        view.endEditing(true)
+    }
+    
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
